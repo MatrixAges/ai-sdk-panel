b1f7952fbb95ec60fb2b5803ac8af253247f4493
1yasa <xiewendao@gmail.com>
2025-10-28 18:09:39 +0800

    fix: ci

--- .github/workflows/test.ts
+++ .github/workflows/test.ts
@@ -18,6 +18,7 @@ export const wf = workflow({
 				{ name: 'Setup Pnpm', uses: 'pnpm/action-setup@v4', with: { version: 'latest' } },
 				setupNode({ 'node-version': 'latest' }),
 				{ name: 'Install Deps', run: 'pnpm i' },
+				{ name: 'Install Playwright', run: 'pnpm exec playwright install' },
 				{ name: 'Build', run: 'pnpm build' },
 				{ name: 'Test', run: 'pnpm run test' }
 			]
